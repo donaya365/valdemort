906452c95354f80d7b13f8a0b148bf7f090040c5
call me kill <82857286+donaya365@users.noreply.github.com>
2024-05-02 06:20:24 +0700

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#         Premium Auto Script By Envyous      #"
+echo -e "================================================="
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By joy Smark                         #"
+echo -e "================================================="                                     
+echo -e "# @envyous                                     #"
+echo -e "# @envyous                                     #"
+echo -e "================================================="
---
+++ ssh/menu.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+clear
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+cyan='\e[36m'
+Lred='\e[91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+cyan='\e[36m'
+Lred='\e[91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+
+clear
+# KERNEL TERBARU
+kernelku=$(uname -r)
+# TOTAL RAM
+total_ram=` grep "MemTotal: " /proc/meminfo | awk '{ print $2}'`
+totalram=$(($total_ram/1024))
+# GETTING DOMAIN NAME
+Domen="$(cat /etc/xray/domain)"
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+IPVPS=$(curl -s ipinfo.io/ip )
+ISPVPS=$( curl -s ipinfo.io/org )
+echo -e ""
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m          ⇱ Subscription Information ⇲          \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\e[33m  Hostname    : $HOSTNAME"
+echo -e "\e[33m  Total RAM   : ${totalram}MB"
+echo -e "\e[33m  Domain      : $Domen"
+echo -e "\e[33m  ISP         : $ISPVPS"
+echo -e "\e[33m  Waktu Aktif : $uphours $upminutes $uptimecek"
+echo -e "\e[33m  NAMA        : JOY SMARK VPN"
+echo -e "\e[33m  Link GP     : https://t.me/joysmark"
+echo -e "\e[33m  Version     : Latest Version"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m          ⇱ AUTO SCRIPT BY ENVYOUS ⇲           \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\033[0m      __   __ ___  ___       "                                 
+echo -e "\033[0m      \ \ / /| _ \/ __|        "                               
+echo -e "\033[0m       \ V / |  _/\__ \       "                  
+echo -e "\033[0m        \_/  |_|  |___/        "                            
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m                ⇱ MENU PANEL ⇲                  \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "$yy 1$y.  SSH & OpenVPN MENU  $wh"
+echo -e "$yy 2$y.  L2TP MENU$wh"
+echo -e "$yy 3$y.  PPTP MENU$wh"
+echo -e "$yy 4$y.  SSTP MENU$wh"
+echo -e "$yy 5$y.  WIREGUARD MENU$wh"
+echo -e "$yy 6$y.  SHADOWSOCKS MENU$wh"
+echo -e "$yy 7$y.  SHADOWSOCKSR MENU$wh"
+echo -e "$yy 8$y.  XRAY VMESS MENU$wh"
+echo -e "$yy 9$y.  XRAY VLESS MENU$wh"
+echo -e "$yy 10$y. XRAY TROJAN MENU$wh"
+echo -e "$yy 11$y. TROJAN GO MENU$wh"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m             ⇱ MENU INFORMATION ⇲               \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "$yy 12$y. CEK SEMUA IP PORT$wh"
+echo -e "$yy 13$y. CEK SEMUA SERVICE VPN$wh"
+echo -e "$yy 14$y. Settings (Pengaturan)$wh"
+echo -e "$yy 15$y. Exit (Keluar)$wh"
+echo -e "${PURPLE}━━━━━━━━━━━━━FROMHELL━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+read -p "Select From Options [ 1 - 15 ] : " menu
+case $menu in
+1)
+clear
+sshovpnmenu
+;;
+2)
+clear
+l2tpmenu
+;;
+3)
+clear
+pptpmenu
+;;
+4)
+clear
+sstpmenu
+;;
+5)
+clear
+wgmenu
+;;
+6)
+clear
+ssmenu
+;;
+7)
+clear
+ssrmenu
+;;
+8)
+clear
+vmessmenu
+;;
+9)
+clear
+vlessmenu
+;;
+10)
+clear
+trmenu
+;;
+11)
+clear
+trgomenu
+;;
+12)
+clear
+ipsaya
+;;
+13)
+clear
+running
+;;
+14)
+clear
+setmenu
+;;
+15)
+clear
+exit
+;;
+*)
+clear
+menu
+;;
+esac
