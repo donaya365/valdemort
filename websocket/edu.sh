1f6a576fc39c33e04ee43427253f726035758e30
call me kill <82857286+donaya365@users.noreply.github.com>
2024-05-02 06:24:20 +0700

    Add files via upload

---
+++ websocket/edu.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# Proxy For Edukasi & Imclass
+# joy
+# ==========================================
+
+# Link Hosting Kalian
+fromhell26="raw.githubusercontent.com/fromhell26/scriptvps/main/websocket"
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-nontls https://${fromhell26}/websocket.py
+chmod +x /usr/local/bin/ws-nontls
+
+# Installing Service
+cat > /etc/systemd/system/ws-nontls.service << END
+[Unit]
+Description=Python Proxy Mod By JoySmark
+Documentation=https://google.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 8880
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-nontls
+systemctl restart ws-nontls
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-ovpn https://${fromhell26}/ws-ovpn.py
+chmod +x /usr/local/bin/ws-ovpn
+
+# Installing Service
+cat > /etc/systemd/system/ws-ovpn.service << END
+[Unit]
+Description=Python Proxy By JoySmark
+Documentation=https://google.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2086
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-ovpn
+systemctl restart ws-ovpn
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-tls https://${fromhell26}/ws-tls
+chmod +x /usr/local/bin/ws-tls
+
+# Installing Service
+cat > /etc/systemd/system/ws-tls.service << END
+[Unit]
+Description=Python Proxy By JoySmsrk
+Documentation=https://google.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 433
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-tls
+systemctl restart ws-tls
