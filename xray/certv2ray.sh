ccc2c11a10d2a3c8cf97ba725bd8cd1fa7e84432
call me kill <82857286+donaya365@users.noreply.github.com>
2024-05-02 09:22:01 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -1,37 +1,99 @@
 #!/bin/bash
-# My Telegram : https://t.me/joysmark
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
+# =========================================
+# Quick Setup | Script Setup Manager
+# Edition : Stable Edition V1.0
+# Auther  : Adit Ardiansyah
+# (C) Copyright 2022
+# =========================================
+
+# // Root Checking
+if [ "${EUID}" -ne 0 ]; then
+		echo -e "${EROR} Please Run This Script As Root User !"
+		exit 1
+fi
+
+# // Exporting Language to UTF-8
+export LC_ALL='en_US.UTF-8' > /dev/null
+export LANG='en_US.UTF-8' > /dev/null
+export LANGUAGE='en_US.UTF-8' > /dev/null
+export LC_CTYPE='en_US.utf8' > /dev/null
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} ERROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+# // Exporting URL Host
+export Server_URL="autosscript.site/ambe"
+export Server_URLL="raw.githubusercontent.com/jaka1m/perizinan/main"
+export Server_Port="443"
+export Server_IP="underfined"
+export Script_Mode="Stable"
+export Auther="geovpn"
+
+# // Root Checking
+if [ "${EUID}" -ne 0 ]; then
+		echo -e "${EROR} Please Run This Script As Root User !"
+		exit 1
+fi
+
+# // Exporting IP Address
+export IP=$( curl -sS ipinfo.io/ip )
+
 clear
+date
+echo ""
 echo start
-sleep 0.5
-source /var/lib/donaya365/ipvps.conf
-domain=$IP
-systemctl enable xray@v2ray-tls
-systemctl enable xray@v2ray-nontls
-systemctl enable xray@vless-tls
-systemctl enable xray@vless-nontls
-systemctl enable xray@trojan
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+#source /var/lib/geovpnstore/ipvps.conf
+domain=$(cat /etc/xray/domain)
+echo -e "[ ${GREEN}INFO${NC} ] Start " 
+sleep 1
+Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
+if [[ ! -z "$Cek" ]]; then
+sleep 1
+echo -e "[ ${red}WARNING${NC} ] Detected port 80 used by ws-nontls" 
+systemctl stop xray@v2ray-nontls
+sleep 2
+echo -e "[ ${GREEN}INFO${NC} ] Processing to stop $Cek " 
+sleep 1
+fi
+echo -e "[ ${GREEN}INFO${NC} ] Starting renew cert... " 
+sleep 2
+echo -e "[ ${GREEN}INFO$NC ] Getting acme for cert"
+wget https://jaka1m.github.io/project/ambe/acme.sh >/dev/null 2>&1
+bash acme.sh --install >/dev/null 2>&1
+bash acme.sh --register-account -m admin@geolstore.net
+wget https://get.acme.sh >/dev/null 2>&1 | sh -s email=admin@geolstore.net
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade >/dev/null 2>&1
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt >/dev/null 2>&1
+/root/.acme.sh/acme.sh --issue -d $domain --standalone --force --keylength ec-256
+/root/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+echo ""
 systemctl restart xray@v2ray-tls
 systemctl restart xray@v2ray-nontls
 systemctl restart xray@vless-tls
 systemctl restart xray@vless-nontls
 systemctl restart xray@trojan
-echo Done
-sleep 0.5
-clear 
-neofetch
+
+sleep 5
+rm acme.sh >/dev/null 2>&1
+echo -e "[ ${GREEN}INFO${NC} ] Renew cert done... "
+restart
